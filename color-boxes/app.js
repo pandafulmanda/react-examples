051e278daabc3b1d0fbe43dacc0ca4f48a8d548c
amanda <pandafulmanda@gmail.com>
2019-11-21 19:43:25 -0600

    updates to use store for app

--- color-boxes/app.js
+++ color-boxes/app.js
@@ -1,4 +1,4 @@
-function ColorBox (props, idx, _allBoxes) {
+function ColorBox (props) {
   const style = {
     border: "2px solid black",
     backgroundColor: props.color,
@@ -12,80 +12,28 @@ function ColorBox (props, idx, _allBoxes) {
   }
 
   return (
-    <div key={idx} style={style}>
+    <div style={style}>
       <button onClick={props.toggleLock}>{btnText}</button>
       <h1>{props.color}</h1>
     </div>
   )
 }
 
-class App extends React.Component {
-  state = {
-    boxes: [
-      this._newBox(),
-      this._newBox(),
-      this._newBox()
-    ]
-  }
-
-  _randomColor() {
-    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-  }
-
-  _newBox() {
-    return {
-      color: this._randomColor(),
-      isLocked: false
-    }
-  }
-
-  randomize() {
-    const currentBoxes = [...this.state.boxes]
-    const newBoxes = currentBoxes.map((box) => {
-      if (!box.isLocked) {
-        box.color = this._randomColor();
-      }
-      return box;
-    })
-    this.setState({boxes: newBoxes})
-  }
-
-  toggleLock(idx) {
-    // TODO: check here that idx is a valid array index
-    const newBoxes = [...this.state.boxes]
-    newBoxes[idx].isLocked = !newBoxes[idx].isLocked
-    this.setState({boxes: newBoxes})
-  }
-
-  addBox() {
-    const newBoxes = [...this.state.boxes, this._newBox()]
-    this.setState({boxes: newBoxes})
-  }
-
-  removeBox() {
-    const newBoxes = [...this.state.boxes]
-    newBoxes.pop()
-    this.setState({boxes: newBoxes})
-  }
-
-  render() {
-    // add the .toggleLock function to each box
-    const boxesWithClickFn = this.state.boxes.map((box, idx) => {
-      box.toggleLock = () => this.toggleLock(idx)
-      return box
-    })
-
-    return (
-      <div className="container">
-          <div className="text-center">
-              <button onClick={() => this.randomize()}>Randomize!</button>
-              <button onClick={() => this.addBox()}>Add Box</button>
-              <button onClick={() => this.removeBox()}>Remove Box</button>
-              <div className="d-flex justify-content-center">
-                {boxesWithClickFn.map(ColorBox)}
-              </div>
-          </div>
+const App = (props) => {
+  return (<div className="container">
+      <div className="text-center">
+        <button onClick={() => myStore.dispatch({type: 'RANDOMIZE'})}>Randomize!</button>
+        <button onClick={() => myStore.dispatch({type: 'ADD_BOX'})}>Add Box</button>
+        <button onClick={() => myStore.dispatch({type: 'REMOVE_BOX'})}>Remove Box</button>
+        <div className="d-flex justify-content-center">
+          {props.boxes.map((box, idx) => (
+            <ColorBox {...box} key={idx} toggleLock={() => myStore.dispatch({
+              type: 'TOGGLE_BOX',
+              boxIndex: idx
+            })}/>
+          ))}
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
--- color-boxes/reducer.js
+++ color-boxes/reducer.js
@@ -1,6 +1,4 @@
-const initialState = {
-  boxes: []
-}
+
 
 const makeRandomColor = () => {
   return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
@@ -17,8 +15,17 @@ const makeDeepCopy = (obj) => {
   return JSON.parse(JSON.stringify(obj))
 }
 
+const initialState = {
+  boxes: [
+    makeNewBox(),
+    makeNewBox(),
+    makeNewBox()
+  ]
+}
+
 const reducer = (state = initialState, action) => {
   const newState = makeDeepCopy(state)
+  console.log(newState)
 
   if (action.type === 'ADD_BOX') {
     return {
@@ -28,10 +35,31 @@ const reducer = (state = initialState, action) => {
       ]
     }
   } else if (action.type === 'REMOVE_BOX') {
+    const newBoxes = [...newState.boxes]
+    newBoxes.pop()
 
-  } else if (action.type === 'LOCK_BOX') {
+    return {
+      boxes: newBoxes
+    }
 
+  } else if (action.type === 'TOGGLE_BOX') {
+    const newBoxes = [...newState.boxes]
+    newBoxes[action.boxIndex].isLocked = !newBoxes[action.boxIndex].isLocked
+    return {
+      boxes: newBoxes
+    }
   } else if (action.type === 'RANDOMIZE') {
-    
+    const currentBoxes = [...newState.boxes]
+    const newBoxes = currentBoxes.map((box) => {
+      if (!box.isLocked) {
+        box.color = makeRandomColor();
+      }
+      return box;
+    })
+    return {
+      boxes: newBoxes
+    }
   }
+
+  return newState
 }
